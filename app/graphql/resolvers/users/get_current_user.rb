c38c0a44f9dfab9f7d2891bcc934a696c5d0551e
Ivan Kuzhelev <vaniki37@gmail.com>
2022-06-05 00:13:00 +0300

    add raise error for currentUser

--- app/graphql/resolvers/users/get_current_user.rb
+++ app/graphql/resolvers/users/get_current_user.rb
@@ -5,6 +5,8 @@ module Resolvers
       type Types::Common::UserType, null: true
 
       def resolve
+        raise GraphQL::ExecutionError, 'Current user is empty' if current_user.blank?
+
         User.find(current_user.id)
       end
     end
